c999eba23521893d94cc7b1d4bc1c95ee877ebfe
otelnov <otelnov@gmail.com>
2015-05-02 23:18:16 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname + '/app',
+  entry: {
+    auth: './components/auth/index.js',
+    common: './index.js'
+  },
+  output: {
+    path: __dirname + '/app/build',
+    filename: '[name].js'
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      NODE_ENV: process.env.NODE_ENV === 'development'
+    }),
+    new webpack.optimize.CommonsChunkPlugin('common', 'common.js')
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        loader: 'eslint',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.html$/,
+        loader: 'raw',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
